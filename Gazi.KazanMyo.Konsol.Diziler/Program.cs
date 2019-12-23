c5ed05c0f11c1cae36233bee095130e2cff31b40
GAZİ TUSAS MYO <gazi.kazan.myo@outlook.com>
2019-12-23 12:07:35 +0300

    Proje dosyası ekle.

---
+++ Gazi.KazanMyo.Konsol.Diziler/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazi.KazanMyo.Konsol.Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int toplam = 0;
+            //int sayac = 0;
+            //Console.WriteLine("Kaç Sayı Girilecek ?");
+            //int isimsayi = byte.Parse(Console.ReadLine());
+            //int[] sayilar = new int[isimsayi];
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    Console.WriteLine((i + 1) + ". Sayıyı Giriniz ? :");
+            //    sayilar[i] = byte.Parse(Console.ReadLine());
+            //    toplam += sayilar[i];
+            //}
+            //Array.Sort(sayilar);
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    Console.WriteLine((i + 1) + ". sayı =" + sayilar[i]);
+            //}
+            //Console.WriteLine("Ortalamanız =" + (toplam / isimsayi));
+            //Console.ReadKey();
+
+            //Console.WriteLine("Kaç Sayı Olacak ?");
+            //int isimsayi = byte.Parse(Console.ReadLine());
+            //int[] sayilar = new int[isimsayi];
+            //Console.WriteLine("Başlangıç Sayısını Belirleyin");
+            //int sayi1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Bitiş Sayısını Belirleyin");
+            //int sayi2 = int.Parse(Console.ReadLine());
+            //Random rnd = new Random();
+
+            //if (sayi1>sayi2)
+            //{
+            //    int temp = sayi1;
+            //    sayi1 = sayi2;
+            //    sayi2 = temp;
+            //}
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+
+            //    int random = rnd.Next(sayi1, sayi2);
+            //    sayilar[i] = random;
+
+
+            //}
+            //Array.Sort(sayilar);
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    Console.WriteLine(sayilar[i]);
+            //}
+
+
+            //Console.WriteLine("Satırı Giriniz");
+            //int satir = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Sütunu Giriniz");
+            //int sutun = int.Parse(Console.ReadLine());
+            //string[,] dizi = new string[satir, sutun];
+            //for (int i = 0; i < dizi.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < dizi.GetLength(1); j++)
+            //    {
+            //        dizi[i, j] = (i + j) % 2 == 0 ? "*" : " ";
+            //    }
+            //}
+            //for (int i = 0; i < dizi.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < dizi.GetLength(1); j++)
+            //    {
+            //        Console.Write(dizi[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            Console.WriteLine("Satırı Giriniz");
+            int satir = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sütunu Giriniz");
+            int sutun = int.Parse(Console.ReadLine());
+            string[,] dizi = new string[satir, sutun];
+
+            for (int i = 0; i < dizi.GetLength(0); i++)
+            {
+                for (int j = 0; j < dizi.GetLength(1); j++)
+                {
+                    Console.WriteLine("*");
+                    
+
+                }
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Gazi.KazanMyo.Konsol.Diziler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Gazi.KazanMyo.Konsol.Diziler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gazi.KazanMyo.Konsol.Diziler")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("1bd3260f-218b-4118-9ef9-7a07ce7b7dda")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
